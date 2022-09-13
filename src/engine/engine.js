aa3965209b4f464d2bba18affeb1285be9925f58
DiyanPv <diyanpetrov61@gmail.com>
2022-09-13 13:42:25 +0300

    Added movement boundaries

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -5,20 +5,27 @@ function renderFrame(state, game) {
   const { spaceshipElement } = game;
   //move player
   if (state.keys.KeyD) {
-    spaceship.posX += movementSpeed;
+    if (spaceship.posX <= window.innerWidth - (state.width + 4)) {
+      spaceship.posX += movementSpeed;
+    }
   }
   if (state.keys.KeyW) {
-    spaceship.posY -= movementSpeed;
+    if (spaceship.posY >= 0) {
+      spaceship.posY -= movementSpeed;
+    }
   }
   if (state.keys.KeyA) {
-    spaceship.posX -= movementSpeed;
+    if (spaceship.posX >= 0) {
+      spaceship.posX -= movementSpeed;
+    }
   }
   if (state.keys.KeyS) {
-    spaceship.posY += movementSpeed;
+    if (spaceship.posY <= window.innerHeight - (state.height + 2)) {
+      spaceship.posY += movementSpeed;
+    }
   }
 
   //render
-  //   game.createSpaceShip().style.left = wizard.posX + `px`;
   spaceshipElement.style.left = spaceship.posX + `px`;
   spaceshipElement.style.top = spaceship.posY + `px`;
 }
